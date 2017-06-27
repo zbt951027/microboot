5b4601b7babc85e4484e2b579c0b7e5c0fac64bf
'zbt' <'839294848@qq.com'>
2017-06-27 21:55:21 +0800

    springBoot的深入，以及thymeleaf模板的使用

--- microboot-advance/src/test/java/cn/mldn/microboot/test/TestSampleController.java
+++ microboot-advance/src/test/java/cn/mldn/microboot/test/TestSampleController.java
@@ -8,10 +8,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import cn.mldn.microboot.Application;
 import cn.mldn.microboot.controller.SampleController;
 import junit.framework.TestCase;
 
-@SpringBootTest(classes={SampleController.class}) 
+@SpringBootTest(classes={Application.class}) 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 public class TestSampleController {
